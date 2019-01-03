045950352b500a0d2369d0a408df9eb01f2e0958
Alexander Rowland <arowland900@gmail.com>
2019-01-03 11:23:17 -0800

    finished solution 3

--- projects/ruby_control_flow/threepio.rb
+++ projects/ruby_control_flow/threepio.rb
@@ -11,3 +11,18 @@ else
       but you don't by any chance go by the alias of Obi-Wan Kenobi, do you? "
 end
 answer = gets.chomp[0].downcase
+
+if answer == "y"
+    puts "Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you!"
+elsif answer == "n"
+puts "I've really enjoyed speaking with you, #{name}, 
+    but if you'll please excuse me, I have to help my friend find 
+    someone named Obi-Wan Kenobi."
+puts "\nWell R2, I suppose we'll just have to keep looking. 
+    R2-D2: (Agreeable droid noises)"
+else
+    puts "I'm sorry, I didn't hear you correctly. I only respond to \"yes\" or \"no\"..."
+    puts "\nWell R2, I suppose we'll just have to keep looking. 
+    R2-D2: (Agreeable droid noises)"
+puts 'other'
+end
