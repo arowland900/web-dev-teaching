97c35c801e7e5ea429fd6171a6a6e67c21d70e6c
Alexander Rowland <arowland900@gmail.com>
2019-01-03 11:22:32 -0800

    continuing solution 3

--- projects/ruby_control_flow/threepio.rb
+++ projects/ruby_control_flow/threepio.rb
@@ -2,3 +2,12 @@ puts "Hello, I am C-3P0, human-cyborg relations."
 
 print "\nAnd your name is? "
 name = gets.chomp
+
+if name == "Obi-Wan Kenobi"
+    puts "Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you!"
+else
+    puts "It is a pleasure to meet you, #{name}."
+    print "I'm terribly sorry for prying,
+      but you don't by any chance go by the alias of Obi-Wan Kenobi, do you? "
+end
+answer = gets.chomp[0].downcase
