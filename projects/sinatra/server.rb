054efc71f22a91d0a7393ba13b2f4b6da627808a
Alexander Rowland <arowland900@gmail.com>
2019-01-04 16:01:05 -0800

    updated server for adding students

--- projects/sinatra/server.rb
+++ projects/sinatra/server.rb
@@ -10,6 +10,13 @@ get '/' do
 end
 
 get "/students/:id" do
-	student = students.select { |s| s.id == params[:id].to_i}
+	student = students.select { |s| s.id == params[:id].to_i }
 	erb :show, :locals => { :student => student[0]}
+end
+
+put "/students/:id" do
+	student = students.select { |s| s.id == params[:id].to_i }
+	student[0].name=params[:name]
+	student[0].email=params[:email]
+	redirect "/students#{params[:id}"
 end
