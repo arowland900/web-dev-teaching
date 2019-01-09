17eb544a105d73cf2109d4f5d90c703eaa684a03
Alexander Rowland <arowland900@gmail.com>
2019-01-09 10:51:43 -0800

    mvc

--- projects/rails_crud/coffeeshop/db/seeds.rb
+++ projects/rails_crud/coffeeshop/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+beans = Bean.create([
+    {name: "Kate's Kona Coffee", roast: "medium", origin: "Kona, Hawaii", quantity: 100},
+    {name: "Mike's Mocha", roast: "light", origin: "California", quantity: 100}
+  ])
